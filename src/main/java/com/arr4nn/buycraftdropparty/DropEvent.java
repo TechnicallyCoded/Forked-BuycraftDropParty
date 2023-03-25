d5fcddf45274582313fdeec12a045ca80783b7ec
TechnicallyCoded <technicallycoded@gmail.com>
2023-03-26 00:09:03 +0100

    Various fixes and optimizations

--- src/main/java/com/arr4nn/buycraftdropparty/DropEvent.java
+++ src/main/java/com/arr4nn/buycraftdropparty/DropEvent.java
@@ -4,43 +4,82 @@ package com.arr4nn.buycraftdropparty;
 import org.bukkit.*;
 import org.bukkit.entity.Firework;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitScheduler;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 
 public class DropEvent {
-    private final int duration;
+
+    private static final int PRE_EVENT_SECONDS = 5;
+    private static final int ENDING_COUNTDOWN_SECONDS = 10;
+
+    // Duration in seconds
+    private final int dropDuration;
     private final Location location;
     private int taskId;
 
     public DropEvent(int duration, Location location) {
-        this.duration = duration;
+        this.dropDuration = duration;
         this.location = location;
     }
 
     public void start() {
-        final int[] ticksLeft = {duration * 20};
-        int interval = Math.max(ticksLeft[0] / 10, 20); // drop items every 2 seconds, but at least 20 ticks
+
+        // Get list of items from configuration file
+        List<String> configItems = BuycraftDropParty.getInstance().getConfig().getStringList("items");
+        int maxItems = BuycraftDropParty.getInstance().getConfig().getInt("max_items");
+        int radius = BuycraftDropParty.getInstance().getConfig().getInt("radius");
+
+        Random random = new Random();
 
         BukkitScheduler scheduler = Bukkit.getScheduler();
-        taskId = scheduler.scheduleSyncRepeatingTask(BuycraftDropParty.getInstance(), new Runnable() {
+        taskId = new BukkitRunnable() {
+            int ticksDuration = PRE_EVENT_SECONDS * 20;
+            final int interval = 20; // drop items every 20 ticks
+
+            boolean dropMode = false;
             int ticks = 0;
 
             @Override
             public void run() {
-                if (ticksLeft[0] == duration * 20) {
-                    Bukkit.broadcastMessage(ChatColor.YELLOW + "Drop party starting in " + duration + " seconds!");
-                } else if (ticksLeft[0] <= interval * 10) {
-                    Bukkit.broadcastMessage(ChatColor.YELLOW + "Drop party ending in " + (ticksLeft[0] / 20) + " seconds!");
+                final int ticksLeft = ticksDuration - ticks;
+                final int secondsLeft = ticksLeft / 20;
+
+                if (ticksLeft <= 0) {
+                    // Done with drop event, stop
+                    if (dropMode) {
+                        this.cancel();
+                        Bukkit.broadcastMessage(ChatColor.YELLOW + "Drop party ended!");
+                    }
+                    // Done with countdown, switch to drop event and reset countdown
+                    else {
+                        ticksDuration = dropDuration * 20;
+                        dropMode = true;
+                    }
+                    return;
                 }
 
-                if (ticks % interval == 0) {
-                    // Get list of items from configuration file
-                    List<String> items = BuycraftDropParty.getInstance().getConfig().getStringList("items");
-                    int maxItems = BuycraftDropParty.getInstance().getConfig().getInt("max_items");
-                    int radius = BuycraftDropParty.getInstance().getConfig().getInt("radius");
+
+                // Chat announcement only once per second
+                if (ticks % 20 == 0) {
+                    // Not in drop mode, count down the start timer
+                    if (!dropMode) {
+                        Bukkit.broadcastMessage(ChatColor.YELLOW + "Drop party starting in " + secondsLeft + " seconds!");
+                    }
+                    // Only show ending countdown when the event is
+                    else if (secondsLeft <= ENDING_COUNTDOWN_SECONDS) {
+                        Bukkit.broadcastMessage(ChatColor.YELLOW + "Drop party ending in " + secondsLeft + " seconds!");
+                    }
+                }
+
+                // Drop items only in drop mode and at the requested interval
+                if (dropMode && ticks % interval == 0) {
+                    System.out.println("spawn!");
+                    List<String> items = new ArrayList<>(configItems);
 
                     // Drop random items within radius
                     for (int i = 0; i < maxItems; i++) {
@@ -48,7 +87,7 @@ public class DropEvent {
                             break;
                         }
 
-                        String itemName = items.remove(new Random().nextInt(items.size()));
+                        String itemName = items.remove(random.nextInt(items.size()));
                         Material itemMaterial = Material.getMaterial(itemName.toUpperCase());
                         if (itemMaterial == null) {
                             continue;
@@ -59,31 +98,22 @@ public class DropEvent {
                         // Play firework effect
                         itemLocation.getWorld().spawn(itemLocation.add(0, 1, 0), Firework.class).detonate();
                         itemLocation.subtract(0, 1, 0);
-                        scheduler.scheduleSyncDelayedTask(BuycraftDropParty.getInstance(), new Runnable() {
-                            @Override
-                            public void run() {
-                                itemLocation.getWorld().spawn(itemLocation.add(0, 1, 0), Firework.class).detonate();
-                                itemLocation.subtract(0, 1, 0);
-                            }
+
+                        // Spawn firework effect 1s later
+                        scheduler.scheduleSyncDelayedTask(BuycraftDropParty.getInstance(), () -> {
+                            itemLocation.getWorld().spawn(itemLocation.add(0, 1, 0), Firework.class).detonate();
                         }, 20L);
-                        scheduler.scheduleSyncDelayedTask(BuycraftDropParty.getInstance(), new Runnable() {
-                            @Override
-                            public void run() {
-                                itemLocation.getWorld().playEffect(itemLocation, Effect.SMOKE, 4);
-                            }
+                        // Play smoke effect 1 block under, 1/2 second later
+                        scheduler.scheduleSyncDelayedTask(BuycraftDropParty.getInstance(), () -> {
+                            itemLocation.subtract(0, 1, 0);
+                            itemLocation.getWorld().playEffect(itemLocation, Effect.SMOKE, 4);
                         }, 30L);
                     }
                 }
 
                 ticks++;
-                ticksLeft[0]--;
-
-                if (ticksLeft[0] <= 0) {
-                    ((BukkitScheduler) scheduler).cancelTask(taskId);
-                    Bukkit.broadcastMessage(ChatColor.YELLOW + "Drop party ended!");
-                }
             }
-        }, 0L, 1L);
+        }.runTaskTimer(BuycraftDropParty.getInstance(), 0L, 1L).getTaskId();
     }
 
     private Location getRandomOffset(double x, double y, double z, int radius) {
